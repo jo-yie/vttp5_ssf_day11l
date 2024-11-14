32520b1f954e1cde85ad5f46834090a16972238b
Jo Yie <joyieleong@gmail.com>
2024-11-14 09:47:10 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5_ssf_day11l/Vttp5SsfDay11lApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5_ssf_day11l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5SsfDay11lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5SsfDay11lApplication.class, args);
+	}
+
+}
